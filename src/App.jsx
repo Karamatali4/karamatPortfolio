e448610a1a4727bca0e36ee1f692a60647e26ab0
Karamatali4 <maqpoon4@gmail.com>
2024-12-02 19:07:29 +0500

    changes in Navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import ErrorPage from './Pages/ErrorPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h2 className='text-red-500'>hi kimi</h2>
-      
+    <> 
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='*' element={<ErrorPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <>
+    <div className="container">
+        <div className="box">
+            <h2>404 Error</h2>
+        </div>
+        <div className="box">
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ErrorPage
