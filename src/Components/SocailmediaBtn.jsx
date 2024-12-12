473a9fb901efd3218322473d54f036409c5a4b6f
Karamatali4 <maqpoon4@gmail.com>
2024-12-12 17:38:22 +0500

    creating socailmedia component and creating socail media buttons

---
+++ src/Components/SocailmediaBtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SocailmediaBtn() {
+    return (
+        <>
+            <div className="box flex gap-4 justify-center items-center mt-2">
+                <button className="btn btn-outline btn-success"> <a target='_blank' href='https://www.linkedin.com/in/karamat-ali-09072a207/'> LinkedIn </a></button>
+                <button className="btn btn-outline btn-success"> <a target='_blank' href='https://github.com/Karamatali4'>GitHub </a></button>
+
+            </div>
+        </>
+    )
+}
+
+export default SocailmediaBtn
