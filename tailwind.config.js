e10d54ad304d85a52865f868cf074d549445c413
Karamatali4 <maqpoon4@gmail.com>
2024-12-16 16:23:05 +0500

    changes in pure css code in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      clipPath: { 'custom-clip': 'polygon(0 0, 100% 0%, 100% 100%, 30% 100%)', },
+    },
   },
   plugins: [
     require('daisyui'),
-  ],
+    function ({ addUtilities }) {
+        const newUtilities = {
+            '.clip-custom-clip': {
+                clipPath: 'polygon(0 0, 100% 0%, 100% 100%, 30% 100%)',
+            },
+        };
+        addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+],
+
   daisyui: {
     themes: ["light", "dark", "cupcake","luxury"],
   },
